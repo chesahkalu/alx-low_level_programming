b2056dd4efe20345755da0e3fbaea333d5dc5a7e
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-08 19:29:09 -0700

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line, using puts function which prints only strings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+ * main function is entry point of program
+ * main returns an int value,0 if no errors
+ * main is void means it has no input arguments for these program
+ */
 int main(void)
-{	
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
-}	
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
