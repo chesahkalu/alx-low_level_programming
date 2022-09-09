a8438c0a0bc7c849a76127b3cc9bc010fa858f5a
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-08 20:11:19 -0700

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art, ,using printf,which prints both strings and variables

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -entry point of program
+ *
+ * Return: zero if no erros, non-zero if error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
