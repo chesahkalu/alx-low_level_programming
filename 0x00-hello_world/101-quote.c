cde21d5f554abca784dfeff0ed91c8f933fde84a
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-08 20:42:17 -0700

    a c program printing without printf and put, and with dates and quotes and hyphen in the print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main - is entry point
+ *
+ * Return: 1, if succesful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
