75c87205e1f7291f2aed131603959154b8d9b4bc
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-10 14:11:52 -0700

    Write a program that prints all possible combinations of single-digit numbers,followed by comma,and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - is entry of program
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+	/*
+	 * prints all single integers, with comma and space after
+	 * loops through 0-9, prints them,prints comma,print space,
+	 * use only putchar
+	 */
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		putchar((x % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
