6383a7482b3b7376a041a8414bbfcea8181a188e
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-15 08:09:49 -0700

    a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+/* This a header file containing declarations
+ * for prototype functions defined in the 0x02-functions_nested_loops
+ * tasks and directory
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+
