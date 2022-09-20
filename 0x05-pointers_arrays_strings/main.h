17f154c4fbdbe781e39e7bd033d712020555dde7
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-19 21:14:28 -0700

    Write a function that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,18 +13,14 @@
 		      if not so, this will cause an error*/
 
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s)
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-
+int _putchar(char c);
 #endif
 
