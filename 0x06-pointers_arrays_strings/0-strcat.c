8f5f7ceff07ac67a34916378735392e7fe023a98
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-22 14:49:27 -0700

    a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,10 @@ char *strcat(char *dest, char *src)
 	int aray = 0, d_count = 0;
 
 	while (dest[aray++])
-	{
 		d_count++;
-	}
 
 	for (aray = 0; src[aray]; aray++)
-	{
 		dest[d_count++] = src[aray];
-	}
+
 	return (dest);
 }
