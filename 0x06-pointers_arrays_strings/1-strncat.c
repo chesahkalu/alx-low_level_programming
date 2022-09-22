7fe8ee6791f1c49d9a28b550ba2009a5b280c42a
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-22 15:56:19 -0700

    Write a function that copies a string.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,6 +23,6 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		dest[d_count++] = src[aray];
 	}
-	
+
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int aray  = 0, src_cnt = 0;
+	int aray  = ; src_cnt = 0;
 
 	while (src[aray++])
 	{
