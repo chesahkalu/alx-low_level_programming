fa9dbee50fb68289805286dd7699e94d0d0dee7a
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-09-22 17:39:58 -0700

    task 5 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *leet - Encodes a string to 1337.
+ * leet - Encodes a string to 1337.
  * @str: string to be encoded.
  *
  * Return: pointer to the encoded leetspeek  string.
@@ -20,5 +20,5 @@ char *leet(char *str)
 			if (str[i] == subs[j])
 				str[i] = le[j / 2];
 	}
-	return (s);
+	return (str);
 }
