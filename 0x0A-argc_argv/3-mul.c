496aaa469c1ec62e77f6e2f6fe5695ba4345a5cd
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-10-02 13:30:49 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,19 +11,19 @@
 
 int main(int argc, char *argv[])
 {
-	int a;
+	int i, n1, n2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = ((atoi(argv[1])) * (atoi(argv[2]));
 
-		printf("%d\n", a);
-	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	i = n1 * n2;
+
+	printf("%d\n", i);
 
 	return (0);
 }
