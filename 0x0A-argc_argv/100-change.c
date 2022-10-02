f8461fe0839597de996274ca0a87871c2c0623be
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-10-02 13:53:58 -0700

    task 4 and 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints minimum number of coins to make change for an amount of money.
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the addition of positive numbers,
+ *
+ * @argc: Argument count/number.
+ * @argv: Argument vector , array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits return 1.
+ *        else return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, b, sum = 0;
+
+	for (n = 1; n < argc; n++)
+	{
+		for (b = 0; argv[n][b] != '\0'; b++)
+		{
+			if (!isdigit(argv[n][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[n]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
