011f785b5503cfb7d7265434c6e926975e9449ae
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-10-13 06:27:42 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -93,7 +93,7 @@ void print_all(const char * const format, ...)
 	{
 		j = 0;
 
-		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+		while (j < 4 && (*(format + i) != *(funcs[j].type)))
 			j++;
 
 		if (j < 4)
