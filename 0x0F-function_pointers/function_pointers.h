39493084d9611c5b64e5c9b77e4db6b797faa9db
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-10-12 19:01:04 -0700

    task 0 & H file ammend

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,7 +10,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
