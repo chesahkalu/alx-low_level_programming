651c49fdbff7f4b19d618c25a21380786e36f666
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-10-31 18:30:36 -0700

    Task 0 and main.h file fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,28 +15,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	ssize_t r, w;
 	char *buffer;
-	ssize_t leno, lenr, lenw;
 
 	if (filename == NULL)
 		return (0);
-
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		close(fd);
 		return (0);
-
-	leno = open(filename, O_RDONLY);
-	lenr = read(leno, buffer, letters);
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-
-	if (leno == -1 || lenr == -1 || lenw == -1 || lenw != lenr)
+	}
+	r = read(fd, buffer, letters);
+	close(fd);
+	if (r == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-
+	w = write(STDOUT_FILENO, buffer, r);
 	free(buffer);
-	close(leno);
-
-	return (lenw);
+	if (r != w)
+		return (0);
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,4 +10,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+#endif
