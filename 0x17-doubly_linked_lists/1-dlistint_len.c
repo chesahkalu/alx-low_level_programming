0c5dda8d7cbdd335aa9bad7aa1c8d0946a94d63b
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-12-08 20:06:31 -0800

    header files and task1 added

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes;
+
+	for (nodes = 0; h != NULL; nodes++)
+		h = h->next;
+	return (nodes);
+}
