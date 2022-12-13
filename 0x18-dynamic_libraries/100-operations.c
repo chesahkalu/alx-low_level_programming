0314ade851c98c9babc49cde40e9b24ef84814c5
Chesachi Victor Kalu <chesahkalu@gmail.com>
2022-12-12 19:55:57 -0800

    task 100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+/*#include <stdlib.h>*/
+/**
+ * add - function that adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: differencr
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: their product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides an integer by anotjjer integer
+ * @a: integer
+ * @b: divisor
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+	
+}
+
+/**
+ * mod - finds the modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
